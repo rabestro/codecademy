35383f6f7eb6bea1da58540cc2910c336d41e588
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-24 23:57:42 +0300

    Added script from "Learn bash scripting"

---
+++ bash-lession/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+first_greeting="Nice to meet you!"
+later_greeting="How are you?"
+greeting_occasion=0
+greeting_limit=$1
+
+while [ $greeting_occasion -lt "$greeting_limit" ]; do
+  if [ $greeting_occasion -lt 1 ]; then
+    echo "$first_greeting"
+  else
+    echo "$later_greeting"
+  fi
+  greeting_occasion=$((greeting_occasion + 1))
+done
