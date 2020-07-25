95cd97797d9cd6c7c56bb0fbcc06dea1eafc67ca
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-25 17:00:23 +0300

    Added first three queries

--- sql-world-populations/population_queries.sql
+++ sql-world-populations/population_queries.sql
@@ -11,11 +11,25 @@ where year = 2005
 
 -- What is the average population of countries in South America in 2003?
 
-
+select avg(population) as "Average population of countries in South America in 2003"
+from population_years
+where year = 2003
+    and country_id in (select id from countries where continent = 'South America');
 
 -- What country had the smallest population in 2007?
 
-
+select name as "The country with the smallest population in 2007"
+from countries
+where id = (
+    select country_id
+    from population_years
+    where year = 2007
+    and population = (
+        select min(population)
+        from population_years
+        where year = 2007
+    )
+);
 
 -- What is the average population of Poland during the time period covered by this dataset?
 
